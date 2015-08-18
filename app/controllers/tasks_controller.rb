eec03518237e4539bf7412d8804ad44e2fd40d80
antonin archer <antonin.archer@hec.edu>
2015-08-18 12:38:45 +0200

    form and strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,9 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(params[:task])
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def edit
@@ -19,12 +22,20 @@ class TasksController < ApplicationController
   end
 
   def update
-    @tasks = Task.find(params[:id])
-    @tasks.? # We'll see that in a moment.
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
   end
 
   def destroy
     @tasks = Task.find(params[:id])
     @tasks.destroy
+  end
+
+  private
+
+  def tasks_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:tasks).permit(:name, :done)
   end
 end
